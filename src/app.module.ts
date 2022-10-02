ca9075df0fe9c3abfbe0774e41c73fbc3fa5191e
Ruslan G <almetfly@gmail.com>
2022-10-02 17:04:13 +0300

    add initital blog module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogModule } from './modules/blog/blog.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [BlogModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
